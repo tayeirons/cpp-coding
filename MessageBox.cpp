06253646302080641f0422b07a9cea4e740812fb
tayeirons <122449581+tayeirons@users.noreply.github.com>
2023-12-08 18:18:17 +0300

    Create MessageBox.cpp

---
+++ MessageBox.cpp
@@ -0,0 +1,8 @@
+#include <windows.h>
+
+int main() {
+    // Открытие окна сообщения
+    MessageBox(NULL, "Hello, Windows!", "Windows Interaction Example", MB_OK | MB_ICONINFORMATION);
+
+    return 0;
+}
